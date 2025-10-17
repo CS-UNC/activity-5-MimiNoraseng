e5c9e7c7f923e0d6fb3ee5d5ed128afa60ab4bf3
Mimi Noraseng <228598446+MimiNoraseng@users.noreply.github.com>
2025-10-17 16:36:56 +0000

    Changes Part One

---
+++ activit_five.py
@@ -0,0 +1,14 @@
+words_file=open('CROSSWD.txt','r')
+
+print(type(words_file))
+
+# print([x for x in dir(words_file) if '_' != x[0]])
+while True:
+    print(words_file.readline())
+    for line in words_file:
+        print(line.strip)
+       #word.append(line)
+# print(words)
+# data=[1,3,2,8,6,5,10]
+
+# print((2*x for x in data if x % 2 == 0))

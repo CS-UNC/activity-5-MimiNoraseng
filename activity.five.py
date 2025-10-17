e704367f8038e1cfe45b41b38c6794d134e53831
Mimi Noraseng <228598446+MimiNoraseng@users.noreply.github.com>
2025-10-17 17:06:41 +0000

    Changes Part Two

--- activity.five.py
+++ activity.five.py
@@ -12,3 +12,15 @@ while True:
 # data=[1,3,2,8,6,5,10]
 
 # print((2*x for x in data if x % 2 == 0))
+
+
+
+
+
+def more_than_20(file):
+    words = []
+    data = open(file,'r')
+words = [x.strip()for x in data if len(x.strip) > 20
+         return words
+
+print(more_than_20("CROSSWRD,txt")

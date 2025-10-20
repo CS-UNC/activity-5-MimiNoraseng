504676cdfa88379278b85b52112bb9c879120963
Mimi Noraseng <228598446+MimiNoraseng@users.noreply.github.com>
2025-10-20 17:00:44 +0000

    Changes Activity Five Process

--- activity_5.py
+++ activity_5.py
@@ -1,25 +1,23 @@
-words_file=open('CROSSWD.txt','r')
-
-print(type(words_file))
-words = []
-for line in words_file:
-    print(line)
-    words.append(line)
-
-print(words)
-print([x for x in dir(words_file)])if '_' != x[0])
-data = [1,3,2,8,5,6,10]
-
-print(2*x for x in data if x % 2 == 0)
-
 def more_than_20(file):
-    words = []
+    word = []
     data = open(file,'r')
     for word in data:
         if len(words.strip()) > 20:
             words.append(words.strip())
-words = [x.strip()for x in data if len(x.strip()) > 20]
+words = [x.strip()for x in data if len(x.strip()) > 20] 
 return words
+# print(more_than_20("CROSSWRD.txt"))
 
+def has_no_e(word):
+    if 'e' in word:
+        return False
+    else:
+        return True
 
-print(more_than_20("CROSSWRD.txt"))
+def uses_only(word,letters):
+    for x in word:
+        if x not in letters:
+            return False
+return True
+
+def all_uses_only(file,letters):

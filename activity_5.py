a60b03aeeba0317b82d60ed3d730f8afddbb2607
Mimi Noraseng <228598446+MimiNoraseng@users.noreply.github.com>
2025-10-22 23:36:28 +0000

    Sumbit

--- activity_5.py
+++ activity_5.py
@@ -4,7 +4,7 @@ def more_than_20(file):
  #for word in data:
 #   if len(word.strip()) > 20:
 #   word = [x.strip()] for x in data if len(x.strip)
-return word
+'return''word'
 
 def has_no_e(word):
     if 'e' in word:
@@ -16,10 +16,10 @@ def uses_only(word,letters):
     for x in word:
         if x not in letters:
             return False
-return True
+'return''True'
 
 def all_uses_only(file,letters):
     for char in word:
         if char not in letters:
             return False
-return True
+'return''True'
